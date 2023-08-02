b6ff29436ddd664ab83db10d0015b3970cbc58cc
yokinanya <1099332511@qq.com>
2023-08-02 15:31:46 +0800

    修改hmcl历史版本的页面样式

--- js/hmcl-history.js
+++ js/hmcl-history.js
@@ -2,34 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
   var stableVersionsTable = document.getElementById("stable-versions");
   var devVersionsTable = document.getElementById("dev-versions");
 
-  function createVersionRow(version, links) {
+  function createVersionRow(version, links, channel) {
     var row = document.createElement("tr");
 
     var versionCell = document.createElement("td");
     versionCell.innerText = version;
     row.appendChild(versionCell);
 
+    var sha1Cell = document.createElement("td");
+    sha1Cell.innerText = "exe：" + links.exesha1 + " \n " + "jar：" + links.jarsha1;
+    row.appendChild(sha1Cell);1
+
     var exeCell = document.createElement("td");
     var exeLink = document.createElement("a");
     exeLink.href = links.exe;
-    exeLink.innerText = "Download (exe)";
+    exeLink.innerText = "hmcl-" + channel + "-" + version + ".exe";
     exeCell.appendChild(exeLink);
     row.appendChild(exeCell);
-
-    var exeSha1Cell = document.createElement("td");
-    exeSha1Cell.innerText = links.exesha1;
-    row.appendChild(exeSha1Cell);
 
     var jarCell = document.createElement("td");
     var jarLink = document.createElement("a");
     jarLink.href = links.jar;
-    jarLink.innerText = "Download (jar)";
+    jarLink.innerText = "hmcl-" + channel + "-" + version + ".jar";
     jarCell.appendChild(jarLink);
     row.appendChild(jarCell);
-
-    var jarSha1Cell = document.createElement("td");
-    jarSha1Cell.innerText = links.jarsha1;
-    row.appendChild(jarSha1Cell);
 
     return row;
   }
@@ -52,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
               return response.json();
             })
             .then(function (data) {
-              var versionRow = createVersionRow(version, data);
+              var versionRow = createVersionRow(version, data, channel);
               table.appendChild(versionRow);
             })
             .catch(function (error) {
